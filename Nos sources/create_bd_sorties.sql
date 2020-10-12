d06d748aa0310fdb99d3aaba585a035b3d296e2f
Carine Léonard <carine.leonard2020@campus-eni.fr>
2020-10-12 13:47:38 +0200

    maj script sql

--- Nos sources/create_bd_sorties.sql
+++ Nos sources/create_bd_sorties.sql
@@ -14,27 +14,27 @@ ALTER TABLE ETAT ADD constraint etat_pk PRIMARY KEY (id)
 CREATE TABLE INSCRIPTION (
     id                              INTEGER NOT NULL,                   -- id créé automatiquement dans entity 
     date_inscription              datetime NOT NULL,                    -- datetime en adéquation avec symfony
-    sortie_no_sortie             INTEGER NOT NULL,
-    participant_no_participant   INTEGER NOT NULL
+    sortie_id             INTEGER NOT NULL,
+    participant_id   INTEGER NOT NULL
 )
 
-ALTER TABLE INSCRIPTION ADD constraint inscription_pk PRIMARY KEY  (SORTIE_no_sortie, PARTICIPANT_no_participant)
+ALTER TABLE INSCRIPTION ADD constraint inscription_pk PRIMARY KEY  (SORTIE_id, PARTICIPANT_id)
 
 
 CREATE TABLE LIEU (
-    no_lieu         INTEGER NOT NULL,
+    id         INTEGER NOT NULL,
     nom_lieu        VARCHAR(30) NOT NULL,
     rue             VARCHAR(30),
     latitude           FLOAT,
     longitude          FLOAT,
-    villes_no_ville   INTEGER NOT NULL,
+    villes_id   INTEGER NOT NULL,
 )
 
-ALTER TABLE LIEU ADD constraint lieu_pk PRIMARY KEY  (no_lieu)
+ALTER TABLE LIEU ADD constraint lieu_pk PRIMARY KEY  (id)
 
 
 CREATE TABLE PARTICIPANT (
-    no_participant   INTEGER NOT NULL,
+    id   INTEGER NOT NULL,
 	pseudo           VARCHAR(30) NOT NULL,
     nom              VARCHAR(30) NOT NULL,
     prenom           VARCHAR(30) NOT NULL,
@@ -46,19 +46,19 @@ CREATE TABLE PARTICIPANT (
 	campus_no_campus    INTEGER NOT NULL
 )
 
-ALTER TABLE PARTICIPANT ADD constraint participant_pk PRIMARY KEY  (no_participant)
+ALTER TABLE PARTICIPANT ADD constraint participant_pk PRIMARY KEY  (id)
 
 ALTER TABLE PARTICIPANT add constraint participantSortie_pseudo_uk unique (pseudo)
 
 CREATE TABLE CAMPUS (
-    no_campus       INTEGER NOT NULL,
+    id             INTEGER NOT NULL,
     nom_campus      VARCHAR(30) NOT NULL
 )
 
-ALTER TABLE CAMPUS ADD constraint campus_pk PRIMARY KEY (no_campus)
+ALTER TABLE CAMPUS ADD constraint campus_pk PRIMARY KEY (id)
 
 CREATE TABLE SORTIE (
-    no_sortie                     INTEGER NOT NULL,
+    id                           INTEGER NOT NULL,
     nom                           VARCHAR(30) NOT NULL,
     datedebut                     smalldatetime NOT NULL,               -- datetime en adéquation avec symfony
     duree                         INTEGER,
@@ -68,54 +68,54 @@ CREATE TABLE SORTIE (
     etatsortie                    INTEGER,
 	urlPhoto                      VARCHAR(250),
     organisateur                  INTEGER NOT NULL,
-    lieu_no_lieu                 INTEGER NOT NULL,
-    etat_no_etat                 INTEGER NOT NULL
+    lieu_id                 INTEGER NOT NULL,
+    etat_id                INTEGER NOT NULL
 )
 
-ALTER TABLE SORTIE ADD constraint sortie_pk PRIMARY KEY  (no_sortie)
+ALTER TABLE SORTIE ADD constraint sortie_pk PRIMARY KEY  (id)
 
 CREATE TABLE VILLE (
-    no_ville      INTEGER NOT NULL,
+    id      INTEGER NOT NULL,
     nom_ville     VARCHAR(30) NOT NULL,
     code_postal   VARCHAR(10) NOT NULL
 )
 
-ALTER TABLE VILLE ADD constraint ville_pk PRIMARY KEY  (no_ville)
+ALTER TABLE VILLE ADD constraint ville_pk PRIMARY KEY  (id)
 
 ALTER TABLE INSCRIPTION
-    ADD CONSTRAINT inscription_participant_fk FOREIGN KEY ( participant_no_participant )
-        REFERENCES participant ( no_participant )
+    ADD CONSTRAINT inscription_participant_fk FOREIGN KEY ( participant_id )
+        REFERENCES participant ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
 ALTER TABLE INSCRIPTIONS
-    ADD CONSTRAINT inscription_sortie_fk FOREIGN KEY ( sortie_no_sortie )
-        REFERENCES sortie ( no_sortie )
+    ADD CONSTRAINT inscription_sortie_fk FOREIGN KEY ( sortie_id)
+        REFERENCES sortie ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
 
 ALTER TABLE LIEU
-    ADD CONSTRAINT lieu_ville_fk FOREIGN KEY ( ville_no_ville )
-        REFERENCES ville ( no_ville )
+    ADD CONSTRAINT lieu_ville_fk FOREIGN KEY ( ville_id )
+        REFERENCES ville ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 	
 ALTER TABLE SORTIE
-    ADD CONSTRAINT sortie_etat_fk FOREIGN KEY ( etat_no_etat )
-        REFERENCES etat ( no_etat )
+    ADD CONSTRAINT sortie_etat_fk FOREIGN KEY ( etat_id )
+        REFERENCES etat ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
 ALTER TABLE SORTIE
-    ADD CONSTRAINT sortie_lieu_fk FOREIGN KEY ( lieu_no_lieu )
-        REFERENCES lieu ( no_lieu )
+    ADD CONSTRAINT sortie_lieu_fk FOREIGN KEY ( lieu_id)
+        REFERENCES lieu ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
 ALTER TABLE SORTIES
     ADD CONSTRAINT sortie_participant_fk FOREIGN KEY ( organisateur )
-        REFERENCES participant ( no_participant )
+        REFERENCES participant ( id )
 ON DELETE NO ACTION 
     ON UPDATE no action 
 
