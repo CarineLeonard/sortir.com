6ff582e046b041bc9ae5606bf6203282b9fb03f1
Carine Léonard <carine.leonard2020@campus-eni.fr>
2020-10-13 17:09:09 +0200

    profil en cours

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\ParticipantType;
+use App\Repository\ParticipantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MainController extends AbstractController
 {
@@ -26,16 +32,35 @@ class MainController extends AbstractController
     /**
      * @Route("/profil", name="main_profil")
      */
-    public function profil()
+    public function profil(ParticipantRepository $participantRepository, Request $request, EntityManagerInterface $em, UserInterface $user)
     {
+        $user = $this->getUser();
+        $participant = $participantRepository -> findOneBy([
+            'mail' => ($user->getUsername())
+        ]);
+        $participant->getNom();
+        $participant->getPrenom();
+        $participant->getPseudo();
+        $participant->getTelephone();
+        $participant->getMail();
+        $participant->getPassword();
+        $participant->getCampus();
         $this->denyAccessUnlessGranted('ROLE_USER');
-        if ('ROLE_USER') {
-            return $this->render('main/profil.html.twig', [
-                'controller_name' => 'MainController',
-            ]);
-        } else {
-            return $this->render('security/login.html.twig');
+        $participantForm = $this->createForm(ParticipantType::class, $participant) ;
+
+
+        $participantForm->handleRequest($request);
+        if ($participantForm->isSubmitted() && $participantForm->isValid()) {
+
+            $participant = $participantForm->getData();
+            $em->persist($participant);
+            $em->flush();
+            $this->addFlash('success', 'Votre profil a bien été mis à jour!');
+            return $this->redirectToRoute('main/index.html.twig', ['idParticipant' => $participant->getIdParticipant(),]);
         }
+        return $this->render('main/profil.html.twig', [
+            'participantForm' => $participantForm->createView()
+        ]);
 
     }
 
--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -2,8 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +18,23 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('mail')
-            ->add('motPasse')
-            ->add('administrateur')
-            ->add('actif')
-            ->add('pseudo')
-            ->add('campus')
-            ->add('sorties')
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('telephone', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('motPasse', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation'],
+            ])
+            ->add('pseudo', TextType::class)
+            ->add('campus', EntityType::class, [
+                'mapped' => false,
+                'class' => Campus::class,
+                'choice_label' => 'nom',
+            ])
         ;
     }
 
