a18eec222225ac29d6c9f77c808af59328a4da21
Lopez Guillaume <guillaume.lopez2020@campus-eni.fr>
2020-10-21 15:05:13 +0200

    Corrections sur profil (encodage du mot de passe)

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -12,24 +12,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ParticipantController extends AbstractController
 {
     /**
      * @Route("/profil", name="participant_profil")
      */
-    public function profil(ParticipantRepository $participantRepository, CampusRepository $campusRepo, Request $request,
-                           EntityManagerInterface $em, UserInterface $user, FileUploader $fileUploader)
+    public function profil(ParticipantRepository $participantRepository, Request $request,
+                           EntityManagerInterface $em, FileUploader $fileUploader, UserPasswordEncoderInterface $encoder)
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
         /** @var Participant $user */
         $user = $this->getUser();
         $participant = $participantRepository -> findOneBy([
-            'mail' => ($user->getUsername())  ]);
-
-        $this->denyAccessUnlessGranted('ROLE_USER');
+            'mail' => ($user->getUsername())
+        ]);
         $participantForm = $this->createForm(ParticipantType::class, $participant) ;
 
         $participantForm->handleRequest($request);
@@ -37,14 +37,14 @@ class ParticipantController extends AbstractController
 
             /** @var UploadedFile $imageFile */
             $imageFile = $participantForm->get('image')->getData();
-            $participant = $participantForm->getData();
 
-
-            if ($imageFile) {
-
+            if ($imageFile)
+            {
                 $imageFileName = $fileUploader->upload($imageFile);
                 $participant->setImageFilename($imageFileName);
             }
+
+            $participant->setMotPasse($encoder->encodePassword($participant, $participant->getPassword()));
 
             // ... persist the $product variable or any other work
             $em->persist($participant);
@@ -61,11 +61,10 @@ class ParticipantController extends AbstractController
     /**
      * @Route("/user/{id}", name="participant_user", requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function user ($id, Request $request)
+    public function user ($id)
     {
         $participantRepo = $this->getDoctrine()->getRepository(Participant::class) ;
         $participant = $participantRepo -> find($id);
-
 
         return $this->render('participant/user.html.twig', [
             'controller_name' => 'ParticipantController',
